fc3a1aa1d33329dea928eee36f6d4dd3c2ef0859
tomE177 <46957425+tomE177@users.noreply.github.com>
2019-02-25 22:33:20 +1100

    Added my Enemy Ai Stuff

---
+++ TheyLurkInTheShadows/Assets/_Scripts/Enemy/AlertSphere.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlertSphere : MonoBehaviour
+{
+    public bool completed = false;
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Bush")
+        {
+            
+            if (gameObject.GetComponentInParent<EnemyController>().hidingSpots.Count == 0)
+            {
+                gameObject.GetComponentInParent<EnemyController>().hidingSpots.Add(other.transform);
+            }
+            else
+            {
+                for (int i = 0; i < gameObject.GetComponentInParent<EnemyController>().hidingSpots.Count; i++)
+                {
+                    if (gameObject.GetComponentInParent<EnemyController>().hidingSpots[i] != other.transform)
+                    {
+                        
+                        gameObject.GetComponentInParent<EnemyController>().hidingSpots.Add(other.transform);
+                        
+                    }
+                }
+            }
+
+        }
+
+        for (int j = 0; j < gameObject.GetComponentInParent<EnemyController>().hidingSpots.Count; j++)
+        {
+            for (int k = 0; k < gameObject.GetComponentInParent<EnemyController>().hidingSpots.Count; k++)
+            {
+                if (k != j)
+                {
+                    if (gameObject.GetComponentInParent<EnemyController>().hidingSpots[j] == gameObject.GetComponentInParent<EnemyController>().hidingSpots[k])
+                    {
+                        gameObject.GetComponentInParent<EnemyController>().hidingSpots.Remove(gameObject.GetComponentInParent<EnemyController>().hidingSpots[j]);
+                    }
+                }
+
+            }
+        }
+
+        if (other.tag == "Enemy")
+        {
+
+            if (gameObject.GetComponentInParent<EnemyController>().nearByEnemies.Count == 0)
+            {
+                gameObject.GetComponentInParent<EnemyController>().nearByEnemies.Add(other.transform);
+            }
+            else
+            {
+                for (int i = 0; i < gameObject.GetComponentInParent<EnemyController>().nearByEnemies.Count; i++)
+                {
+                    if (gameObject.GetComponentInParent<EnemyController>().nearByEnemies[i] != other.transform)
+                    {
+
+                        gameObject.GetComponentInParent<EnemyController>().nearByEnemies.Add(other.transform);
+
+                    }
+                }
+            }
+
+        }
+
+        for (int j = 0; j < gameObject.GetComponentInParent<EnemyController>().nearByEnemies.Count; j++)
+        {
+            for (int k = 0; k < gameObject.GetComponentInParent<EnemyController>().nearByEnemies.Count; k++)
+            {
+                if (k != j)
+                {
+                    if (gameObject.GetComponentInParent<EnemyController>().nearByEnemies[j] == gameObject.GetComponentInParent<EnemyController>().nearByEnemies[k])
+                    {
+                        gameObject.GetComponentInParent<EnemyController>().nearByEnemies.Remove(gameObject.GetComponentInParent<EnemyController>().nearByEnemies[j]);
+                    }
+                }
+
+            }
+        }
+        if (!completed)
+        {
+            completed = true;
+        }
+        
+    }
+}
+
+    
+
---
+++ TheyLurkInTheShadows/Assets/_Scripts/Enemy/Sight.cs
@@ -0,0 +1,213 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sight : MonoBehaviour
+{
+    public GameObject self;
+    public List<Transform> inSight = new List<Transform>();
+    public GameObject sightSinc;
+    public Material green;
+    EnemyController states;
+    public LayerMask layerMask = ~(1 << 10);
+    public LayerMask layerMask2 = ~(1 << 10);
+    // Use this for initialization
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(inSight.Count <= 0)
+        {
+            GetComponentInParent<EnemyController>().playerInSight = false;
+        }
+
+        if (inSight.Count > 0 && GetComponentInParent<EnemyController>().state != EnemyController.State.Dead)
+        {
+            
+            for (int i = 0; i < inSight.Count; i++)
+            {
+                
+                RaycastHit hit;
+                LayerMask LM = layerMask;
+                if(GetComponentInParent<EnemyController>().state != EnemyController.State.Guard && GetComponentInParent<EnemyController>().state != EnemyController.State.Patrol)
+                {
+                     LM = layerMask2;
+                }
+                else
+                {
+                     LM = layerMask;
+                }
+                
+
+                if (Physics.Raycast(this.sightSinc.transform.position, inSight[i].GetComponentInChildren<Transform>().position - this.sightSinc.transform.position, out hit, Mathf.Infinity, LM))
+                {
+                    
+
+                    Debug.DrawRay(this.sightSinc.transform.position, (inSight[i].GetComponentInChildren<Transform>().position - this.sightSinc.transform.position) * hit.distance, Color.red);
+                    Debug.Log("hit: " + hit.collider.gameObject + " im: " + gameObject.name);
+                    
+                    switch (hit.collider.GetComponent<Transform>().tag)
+                    {
+                        
+                        case "Player":
+
+                            if (hit.collider.tag == "Player")
+                            {
+                                if (hit.collider.GetComponent<PController>().hidden == false)
+                                {
+                                    Debug.Log("Player");
+                                    GetComponentInParent<EnemyController>().playerInSight = true;
+                                }
+                                else
+                                {
+                                    if(GetComponentInParent<EnemyController>().state == EnemyController.State.Searching && GetComponentInParent<EnemyController>().searchingSpot)
+                                    {
+                                        Debug.Log("Player Found");
+                                        GetComponentInParent<EnemyController>().playerInSight = true;
+                                        GetComponentInParent<EnemyController>().state = EnemyController.State.Chase;
+
+
+                                    }
+                                    else
+                                    {
+                                        Debug.Log("Player Hidden");
+                                    }
+                                    
+                                }
+
+                            }
+                            break;
+
+                        case "Enemy":
+                            GameObject hitEnemy = hit.transform.gameObject;
+                            
+                            if (hit.collider.tag == "Enemy")
+                            {
+
+                                //Debug.Log("seen enemy1");
+                                if (hit.transform.GetComponent<EnemyController>().Hidden == false)
+                                {
+                                    //Debug.Log("seen enemy2");
+                                    if (hitEnemy!= null)
+                                    {
+                                        //Debug.Log("seen enemy3");
+                                        if (hitEnemy.GetComponent<EnemyController>().state == EnemyController.State.Dead )
+                                        {
+                                            //Debug.Log("seen enemy4");
+                                            if(GetComponentInParent<EnemyController>().spottedDeadEnemies.Count <= 0)
+                                            {
+                                                //Debug.Log("seen enemy5");
+                                                if(GetComponentInParent<EnemyController>().state == EnemyController.State.Guard || GetComponentInParent<EnemyController>().state == EnemyController.State.Patrol) 
+                                                    GetComponentInParent<EnemyController>().state = EnemyController.State.Alert;
+
+                                                GetComponentInParent<EnemyController>().posTarget = hit.collider.transform.position;
+                                                GetComponentInParent<EnemyController>().spottedDeadEnemies.Add(hitEnemy);
+                                            }
+                                            else
+                                            {
+                                                //Debug.Log("seen enemy6");
+                                                int check = 0;
+                                                for (int k = 0; k < GetComponentInParent<EnemyController>().spottedDeadEnemies.Count; k++)
+                                                {
+                                                    if (GetComponentInParent<EnemyController>().spottedDeadEnemies[k] != hitEnemy)
+                                                    {
+                                                        check += 1;
+                                                    }
+                                                }
+                                                if(check == GetComponentInParent<EnemyController>().spottedDeadEnemies.Count)
+                                                {
+                                                    //Debug.Log("seen enemy7");
+                                                    if (GetComponentInParent<EnemyController>().state == EnemyController.State.Guard || GetComponentInParent<EnemyController>().state == EnemyController.State.Patrol)
+                                                        GetComponentInParent<EnemyController>().state = EnemyController.State.Alert;
+
+                                                    GetComponentInParent<EnemyController>().posTarget = hit.collider.transform.position;
+                                                    GetComponentInParent<EnemyController>().spottedDeadEnemies.Add(hitEnemy);
+                                                }
+                                                //Debug.Log("seen enemy8");
+                                            }
+                                            
+                                            
+                                        }
+                                    }
+                                    
+                                }
+                                
+
+                            }
+
+                            break;
+                        default:
+                            Debug.Log("in switch but no matching tags");
+                            GetComponentInParent<EnemyController>().playerInSight = false;
+                            break;
+
+                        
+
+                    }
+
+                    Debug.Log("Raycast4");
+
+                }
+                else
+                {
+                    Debug.DrawRay(this.sightSinc.transform.position, (inSight[i].GetComponentInChildren<Transform>().position - this.sightSinc.transform.position) * 100, Color.red);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" || other.tag == "Enemy")
+        {
+
+            Debug.Log("Player in trigger");
+            if (other.transform.GetChild(0) != self)
+            {
+                if (inSight.Count <= 0)
+                {
+                    inSight.Add(other.transform.GetChild(0));
+                }
+                else
+                {
+                    for (int i = 0; i <= inSight.Count; i++)
+                    {
+                        
+                        if (other.transform.GetChild(0) != inSight[i])
+                        {
+                            inSight.Add(other.transform.GetChild(0));
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.tag == "Player" || other.tag == "Enemy")
+        {
+            for (int i = 0; i < inSight.Count; i++)
+            {
+                if (inSight[i].GetComponentInParent<Transform>().tag == "Player")
+                {
+                    Debug.Log("inside player sight if");
+                    gameObject.GetComponentInParent<EnemyController>().playerInSight = false;
+                }
+
+                if (other.transform.GetChild(0) == inSight[i])
+                {
+                    
+                    inSight.Remove(other.transform.GetChild(0));
+                }
+            }
+
+        }
+    }
+
+    
+
+
+}
