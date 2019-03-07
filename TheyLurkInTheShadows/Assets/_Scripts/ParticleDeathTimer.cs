db274e7246db20cb57f8cfddc78c9f83cc3e9cf0
Tom Collyer <37063689+para-D0X@users.noreply.github.com>
2019-03-07 20:09:12 +1100

    Added Particle Effects
     + Added Blink Effect to blink ability

---
+++ TheyLurkInTheShadows/Assets/_Scripts/ParticleDeathTimer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDeathTimer : MonoBehaviour
+{
+
+    public float deathTimer;
+
+    void Start()
+    {
+        gameObject.transform.parent = null;
+        Destroy(gameObject, deathTimer);
+    }
+
+
+}
