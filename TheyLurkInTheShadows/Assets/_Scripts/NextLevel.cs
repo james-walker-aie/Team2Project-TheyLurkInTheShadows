da9b2eb4eb8df19bfc231df74dfbd6e724c9949b
SianSallway <37866416+SianSallway@users.noreply.github.com>
2019-03-27 19:09:47 +1100

    saving, loading levels etc.
    removed unessicary scenes, adding button scripts and loading scripts to be added as component to game objects objects

---
+++ TheyLurkInTheShadows/Assets/_Scripts/NextLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider hit)
+    {
+        if (hit.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+    }
+}
