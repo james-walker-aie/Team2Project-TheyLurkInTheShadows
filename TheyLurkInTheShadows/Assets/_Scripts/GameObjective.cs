ea363b6cf9c514fd7ba1e8b5461869e7244e26ed
lachie12435 <lachm04@gmail.com>
2019-04-03 20:41:30 +1100

    STUFF HAPPENED

---
+++ TheyLurkInTheShadows/Assets/_Scripts/GameObjective.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjective : MonoBehaviour
+{
+    public bool ObjectHasBeenCollected = false;
+    public GameObject Door;
+
+    public void Start()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Update()
+    {
+        gameObject.transform.Rotate(0, 15 * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.CompareTag("Player"))
+        {
+            ObjectHasBeenCollected = true;
+            Door.GetComponent<Collider>().enabled = false;
+            Door.SetActive(false);
+            gameObject.SetActive(false);           
+        }
+    }
+}
