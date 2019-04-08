110079271d6d79ada8a2f6f1adddf96c24f2afcd
SianSallway <37866416+SianSallway@users.noreply.github.com>
2019-04-08 21:45:01 +1000

    Checkpoint and menu
    Re doing checkpoint code and adding script to the retry button to reload the current scene/level

---
+++ TheyLurkInTheShadows/Assets/_Scripts/PlayerCheckpoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCheckpoint : MonoBehaviour
+{
+    private CheckpointManager checkMan;
+
+    void Start()
+    {
+        checkMan = GameObject.FindGameObjectWithTag("CheckMan").GetComponent<CheckpointManager>();
+    }
+
+    public void OnTriggerEnter(Collider hit)
+    {
+        if (hit.CompareTag("Player"))
+        {
+            checkMan.checkpointPos = transform.position;
+        }
+
+    }
+}
